4dc656f676dcc28d3d73c4fa20808af65ad402d3
Alexander Kuzmin <kuzminalex96@icloud.com>
2024-10-04 23:30:07 +0200

    Finish work, only need to download to S42 and push

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+void sort(int *arr, int size) {
+    int i = 0;
+    while (i < size - 2) {
+        if(arr[i] > arr[i+1]) 
+        {
+            int contemporary = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = contemporary;
+        } 
+        i++;
+    }
+}
+
+void ft_sort_int_tab(int *tab, int size)
+{
+    int i = 0;
+    while (i < size)
+    {
+        sort(tab, size);
+        i++;
+    }
+}
+
+
+/*
+//Test
+void printed(int *arr, int size)
+{
+    int i = 0;
+    write(1, "{", 1);
+    while (i < size)
+    {
+        char ch[3];
+        ch[0] = arr[i] + '0';
+        ch[1] = ',';
+        ch[2] = ' ';
+        write(1, &ch, 2);
+        i++;
+    }
+    write(1, "}", 1);
+}
+
+int main(void)
+{
+	int arr[] = {3,1,7,8,2,4,6,5,9};
+    //if work shows: {1, 2, 3, 4, 5, 6, 7, 8, 9, }
+	ft_sort_int_tab( arr, 9 );
+    printed(arr, 9);
+	return (0);
+}
+*/
