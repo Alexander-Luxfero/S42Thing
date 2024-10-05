89bec8204b2d6a30114dd680deff4532f59fcd4c
Alexander Kuzmin <kuzminalex96@icloud.com>
2024-10-05 21:15:19 +0200

    Added ex09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -7,12 +7,14 @@ char *ft_strcapitalize(char *str)
 
     while (str[i] != '\0')
     {
-        if (first_char != 0 && (str[i] >= 'a' && str[i] <= 'z')){
+        if (first_char != 0 && (str[i] < '9' && str[i] > '0')) {
+            first_char = 0;
+        } else if (first_char != 0 && (str[i] >= 'a' && str[i] <= 'z')){
             str[i] = str[i] - 32;
             first_char = 0;
         } else if ( str[i] >= 'A' && str[i] <= 'Z'){
             str[i] = str[i] + 32;
-        } else if (str[i] == ' '){
+        } else if (str[i] == ' ' || str[i] == '+' || str[i] == '-'){
             first_char = 1;
         }
         i++;
@@ -24,7 +26,7 @@ char *ft_strcapitalize(char *str)
 int main(void)
 {
     //If Ok print 0 first time and 1 second!
-    char text[18] = "wHere is tHe wiLl";
+    char text[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
     ft_strcapitalize(text);
     printf("If OK: %s\n", text);
     return (0);
