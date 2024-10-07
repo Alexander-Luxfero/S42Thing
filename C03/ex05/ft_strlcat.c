d2886f413d152a74e49e6442a2f2ba8d6ce6c01a
Alexander Kuzmin <kuzminalex96@icloud.com>
2024-10-07 03:30:32 +0200

    Done C03 from ex00 to ex05

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,40 @@
+unsigned int str_length(char *str) {
+    unsigned int length = 0;
+    while (str[length] != '\0')
+        length++;
+    return length;
+}
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size) {
+    unsigned int length_dest = str_length(dest);
+    unsigned int length_src = str_length(src);
+    unsigned int i = 0;
+
+    if (size <= length_dest)
+        return size + length_src;
+
+    while (src[i] != '\0' && (length_dest + i) < (size - 1)) {
+        dest[length_dest + i] = src[i];
+        i++;
+    }
+
+    dest[length_dest + i] = '\0';
+    return length_dest + length_src;
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char str1[50] = "String first, ";
+    char str2[] = "concated with second, and this is gone.";
+    char str3[50] = "String first, ";
+
+    ft_strlcat(str1, str2, 36);
+    strlcat(str3, str2, 36);
+    printf("%s", strcmp(str1, str3) == 0 ? "OK!\n" : "KO!\n");
+    printf("Custom result: %s|||END|||\n", str1);
+    printf("Standart result: %s|||END|||\n", str3);
+    return 0;
+}
+*/
