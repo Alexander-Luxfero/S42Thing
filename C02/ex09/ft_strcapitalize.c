1e0d95ac9feb4df22c0854f461c2be9116bfca4b
Aleksandr Kuzmin <akuzmin@c4b5c2.42berlin.de>
2024-10-08 19:11:38 +0200

    Finish C02 ex09, go next at home

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: akuzmin <akuzmin@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:24:00 by akuzmin           #+#    #+#             */
-/*   Updated: 2024/10/08 12:28:18 by akuzmin          ###   ########.fr       */
+/*   Updated: 2024/10/08 19:07:50 by akuzmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@ int	is_alpha(char c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
+int     is_num(char c)
+{
+        return (c >= '0' && c <= '9');
+}
+
 
 char	*ft_strcapitalize(char *str)
 {
@@ -24,25 +30,28 @@ char	*ft_strcapitalize(char *str)
 	first_char = 1;
 	while (str[i] != '\0')
 	{
-		if (is_alpha(str[i]))
+		if (is_alpha(str[i]) || is_num(str[i]))
 		{
-			if (first_char && str[i] >= 'a' && str[i] <= 'z')
+			if (first_char)
 			{
 				str[i] -= 32;
 				first_char = 0;
 			}
-			else if (!first_char && str[i] >= 'A' && str[i] <= 'Z')
-				str[i] += 32;
 			else
-				first_char = 0;
+			{
+				str[i] += 32;
+			}
+		
 		}
-		else if (!(str[i] >= '0' && str[i] <= '9'))
+		else
+		{
 			first_char = 1;
+		}
 		i++;
 	}
 	return (str);
 }
-/*
+
 #include <stdio.h>
 #include <string.h>
 //Test
@@ -55,4 +64,4 @@ int main(void)
     printf("%s\n", strcmp(t, c) ? "OK!" : "KO!");
     return (0);
 }
-*/
+
