e18d5e02c913ca9b1cbc6dd7d6c935a3e09fb9f6
Alexander Kuzmin <kuzminalex96@icloud.com>
2024-10-17 02:47:19 +0200

    Create ex04 in C07. That was is to hard and long

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuzmin <akuzmin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 16:24:25 by akuzmin           #+#    #+#             */
+/*   Updated: 2024/10/15 13:29:59 by akuzmin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	base_l(char *base);
+int	check_err(char *base);
+long long	calculate(char *str, char *base, unsigned int i, long long output);
+int	get_num_len(long long nb, int base_len);
+
+long long	ft_atoi_base(char *str, char *base_from)
+{
+	unsigned int	i;
+	long long	sign;
+	long long	out;
+
+	i = 0;
+	out = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i])
+	{
+		if (calculate(str, base_from, i, out) == out && base_l(base_from) > 1)
+			break;
+		out = calculate(str, base_from, i, out);
+		i++;
+	}
+	return ((sign * out));
+}
+
+char	*ft_putnbr_base(long long nb, char *base_to, char *temp)
+{
+	int	b_len;
+	
+	b_len = base_l(base_to);
+	if (nb >= b_len)
+		temp = ft_putnbr_base(nb / b_len, base_to, temp);
+	*temp = base_to[nb % b_len];
+	return (temp + 1);
+}
+
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	char *str_out;
+	char *temp;
+	long long	nb;
+	int bl_to;
+
+	if (check_err(base_from) == 0 || check_err(base_to) == 0)
+		return (NULL);
+	bl_to = base_l(base_to);
+	nb = ft_atoi_base(nbr, base_from);
+	str_out = malloc((get_num_len(nb, bl_to) + 1) * sizeof(char));
+	if (!str_out)
+		return (NULL);
+	temp = str_out;
+	if (nb < 0)
+	{
+		*temp++ = '-';
+		nb = -nb;
+	}
+
+	temp = ft_putnbr_base(nb, base_to, temp);
+	*temp = '\0';
+	return (str_out);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	//
+	printf("\n////////////////////////////////////////////////////////////\n");
+	printf("Input: %s\nBase From: %s\nBase To: %s\nOutput: %s\n", "101101", "01",\
+	"0123456789", ft_convert_base("101101", "01", "0123456789"));
+	printf("%s\n", strcmp(ft_convert_base("101101", "01", "0123456789"), "45") == 0 ? \
+	 "OK!" : "KO!");
+	//
+	printf("\n////////////////////////////////////////////////////////////\n");
+	printf("Input: %s\nBase From: %s\nBase To: %s\nOutput: %s\n", \
+	"   +---101101", "01", "0123456789", ft_convert_base("   +---101101", \
+	"01", "0123456789"));
+	printf("%s\n", strcmp(ft_convert_base("   +---101101", "01", "0123456789"), "-45") == 0 ? "OK!" : "KO!");
+	//
+	printf("\n////////////////////////////////////////////////////////////\n");
+	printf("Input: %s\nBase From: %s\nBase To: %s\nOutput: %s\n", "", "01",\
+	"0123456789", ft_convert_base("", "01", "0123456789"));
+	printf("%s\n", strcmp(ft_convert_base("", "01", "0123456789"), "") == 0 ? \
+	 "OK!" : "KO!");
+	//
+	printf("\n////////////////////////////////////////////////////////////\n");
+	printf("Input: %s\nBase From: %s\nBase To: %s\nOutput: %s\n",\
+	"+++---+--123643", "0123456789", "0123456789abcdef", \
+	ft_convert_base("+++---+--123643", "0123456789", "0123456789abcdef"));
+	printf("%s\n", strcmp(ft_convert_base("+++---+--123643", "0123456789", \
+	"0123456789abcdef"), "-45") == 0 ? "OK!" : "KO!");
+	//
+	return (0);
+}
+*/
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuzmin <akuzmin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 16:24:25 by akuzmin           #+#    #+#             */
+/*   Updated: 2024/10/15 13:28:20 by akuzmin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	base_l(char *base)
+{
+	int	len;
+
+	len = 0;
+	while (base[len] != '\0')
+		len++;
+	return (len);
+}
+
+int	get_num_len(long long nb, int base_len)
+{
+	int	len = 0;
+
+	if (nb <= 0)
+		len++;
+	while (nb)
+	{
+		nb = nb / base_len;
+		len++;
+	}
+	return (len);
+}
+
+long long	calculate(char *str, char *base, unsigned int i, long long output)
+{
+	int	j;
+
+	j = 0;
+	while (j < base_l(base))
+	{
+		if (base[j] == str[i])
+		{
+			output = output * base_l(base) + j;
+			break;
+		}
+		j++;
+	}
+	return (output);
+}
+
+int	check_err(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (base_l(base) <= 1)
+		return (0);
+	while (i < base_l(base))
+	{
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
