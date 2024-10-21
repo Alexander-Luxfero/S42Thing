e38ae395ea78405e92766a7c747bba7e6def8dad
Aleksandr Kuzmin <akuzmin@c4c5c1.42berlin.de>
2024-10-21 13:24:45 +0200

    Add rush-02 to whole projects

--- Rush-02/ex00/read_dict.c
+++ Rush-02/ex00/read_dict.c
@@ -3,28 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   read_dict.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adeaconu <adeaconu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gikarcev <gikarcev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 11:54:48 by gikarcev          #+#    #+#             */
-/*   Updated: 2024/10/19 16:57:17 by adeaconu         ###   ########.fr       */
+/*   Updated: 2024/10/20 20:26:58 by gikarcev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//read the dictionary until the \n and return the whole line
-	//open the file
-	//read(1) char by char until we encounter \n
-	//and keep track of how many chars we read
-	//malloc a string with the necesaqry lenght
-	//we read again with the exact number of chars
 
 #include "rush.h"
 
 char	*read_dict(int fd)
 {
-	int	i;
-	char	
-		*line;
-	int	bytes_read;
+	int		i;
+	char	*line;
+	int		bytes_read;
 
 	i = 1;
 	bytes_read = 0;
@@ -33,7 +25,10 @@ char	*read_dict(int fd)
 		return (NULL);
 	bytes_read = read(fd, &line[i - 1], 1);
 	if (bytes_read == 0)
+	{
+		free(line);
 		return (NULL);
+	}
 	while (line[i - 1] != '\n')
 	{
 		bytes_read = read(fd, &line[i], 1);
@@ -44,27 +39,3 @@ char	*read_dict(int fd)
 	line[i - 1] = '\0';
 	return (line);
 }
-
-// int main(void)
-// {	
-// 	char *line;
-// 	char *path;
-// 	int fd;
-// 	path = "./numbers.dict";
-// 	fd = open(path, O_RDONLY);
-// 	if (fd <= 0)
-// 	{
-// 		ft_putstr("Dict Error\n");
-// 		return (1);
-// 	}	
-// 	line = read_dict(fd);
-// 	while (line)
-// 	{
-// 		line = read_dict(fd);
-// 		if (!line)
-// 			return (0);
-// 		printf("%s\n", line);
-// 	}
-// 	free(line);
-// 	close(fd);
-//}
---
+++ Rush-02/ex00/utils2.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gikarcev <gikarcev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 16:03:01 by gikarcev          #+#    #+#             */
+/*   Updated: 2024/10/20 18:44:34 by gikarcev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		length;
+	int		i;
+
+	length = 0;
+	while (src[length])
+		length++;
+	copy = malloc((length + 1) * sizeof(char));
+	if (!copy)
+		return (NULL);
+	i = -1;
+	while (++i < length)
+		copy[i] = src[i];
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*get_substr(char *str, int start, int length)
+{
+	char	*substr;
+
+	if (start == 0 && length == 0)
+		return (str);
+	substr = (char *)malloc(4 * sizeof(char));
+	ft_strncpy(substr, str + start, length); 
+	substr[length] = '\0'; 
+	return (substr);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	unsigned int	length_str1;
+	unsigned int	i;
+
+	length_str1 = 0;
+	while (dest[length_str1] != '\0')
+		length_str1++;
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[length_str1 + i] = src[i];
+		i++;
+	}
+	dest[length_str1 + i] = '\0';
+	return (dest);
+}
+
+char	*power_of_10(unsigned int length)
+{
+	char			*output;
+	unsigned int	i;
+
+	output = (char *)malloc((length + 1) * sizeof(char));
+	output[0] = '1';
+	i = 1;
+	while (i < length)
+	{
+		output[i] = '0';
+		i++;
+	}
+	output[i] = '\0';
+	return (output);
+}
