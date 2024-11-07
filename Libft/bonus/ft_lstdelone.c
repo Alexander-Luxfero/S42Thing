03afa2060076185dfa8ff4d520269fd8a706c2d4
Alexander Kuzmin <kuzminalex96@icloud.com>
2024-11-07 15:35:38 +0100

    Done bonus

--- Libft/bonus/ft_lstdelone.c
+++ Libft/bonus/ft_lstdelone.c
@@ -1,28 +1,35 @@
 #include "libft.h"
-
-typedef struct s_list
-{
-void *content;
-struct s_list *next;
-} t_list;
 /*
 1) Взять ноды и адрес к елементу в ноде для ужаления
 2) Пройтись по ноде пока не найду что что нужно удалить при помощи free
 3) Соеденить цепочку обратно
 */
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+// void ft_lstdelone(t_list *lst, void (*del)(void *))
+// {
+// 	t_list *previous;
+// 	t_list *after;
+
+// 	if (lst == NULL)
+// 		return ;
+// 	while (lst != NULL)
+// 	{
+// 		previous = lst;
+// 		if(lst->next == del)
+// 		{
+// 			lst = lst->next;
+// 			after = lst->next;
+// 			free(lst);
+// 			previous->next = after;
+// 			lst = previous;
+// 		}
+// 		lst = lst->next;
+// 	}
+// }
+//*del это функция которая делает все это сама, зря парился
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list *previous;
-	t_list *after;
-
-	if (lst == NULL)
-		return ;
-	while (lst != NULL)
-	{
-		after = lst;
-		if(lst->next == del)
-		{
-
-		}
-	}
+    if (!lst || !del)
+        return;
+    del(lst->content);
+    free(lst);
 }
---
+++ Libft/bonus/ft_lstmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new_list;
+	t_list *new_elem;
+
+	new_list = NULL;
+	while(lst != NULL)
+	{
+		new_elem = malloc(sizeof(t_list));
+		if (!new_elem)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		new_elem->content = f(lst->content);
+		new_elem->next = NULL;
+		ft_lstadd_back(&new_list, new_elem);
+		lst = lst->next;
+	}
+	return (new_list);
+}
