b977d5c547080f66a5ed85bc84c0a5436cda3085
Aleksandr Kuzmin <akuzmin@c4c6c2.42berlin.de>
2024-11-14 14:58:05 +0100

    Fixed problems with pointers, decimat, chars in printf but for some reason abears more in enother sections

--- ft_printf/src/ft_print_pointer.c
+++ ft_printf/src/ft_print_pointer.c
@@ -3,37 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_pointer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akuzmin <akuzmin@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: akuzmin <akuzmin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 21:14:25 by akuzmin           #+#    #+#             */
-/*   Updated: 2024/11/14 11:40:16 by akuzmin          ###   ########.fr       */
+/*   Updated: 2024/11/14 14:46:11 by akuzmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static size_t	ft_use_that_shit(char *str)
+{
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = 0;
+	while (i < 2)
+	{
+		len += ft_putchar(str[i]);
+		i++;
+	}
+	while (str[i])
+	{
+		while (str[i] == '0' && len == 2)
+			i++;
+		len += ft_putchar(str[i]);
+		i++;
+	}
+	return (len);
+}
 
 size_t	ft_print_pointer(void *ptr)
 {
 	unsigned long	address;
 	char			*base16;
 	int				i;
-	char			hex_address[16];
+	char			*hex_address;
+	size_t			len;
 
+	hex_address = malloc(19);
+	hex_address[18] = '\0';
 	address = (unsigned long)ptr;
 	base16 = "0123456789abcdef";
-	write(1, "0x", 2);
-	if (ptr == NULL)
-	{
-		write(1, "0", 1);
-		return (3);
-	}
-	i = 15;
-	while (i >= 0)
+	if (ptr == (NULL))
+		return (ft_putstr("(nil)"));
+	i = 17;
+	hex_address[0] = '0';
+	hex_address[1] = 'x';
+	while (i >= 2)
 	{
 		hex_address[i] = base16[address % 16];
 		address /= 16;
 		i--;
 	}
-	write(1, hex_address, 9);
-	return (18);
+	len = ft_use_that_shit(hex_address);
+	free(hex_address);
+	return (len);
 }
